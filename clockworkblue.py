325523c58db16e0373ff93d07a3edb666442ed57
LukeLukasson <lukelukasson@gmail.com>
2015-07-10 13:12:36 +0200

    countdown and led simulator implemented

--- clockworkblue.py
+++ clockworkblue.py
@@ -1,5 +1,31 @@
+#!/usr/bin/python
+#
 # Milestones:
 #
-#	o	Count down and simulate LEDs
+#	x	Count down and simulate LEDs
 #	o	Include smart cycles including working hours and amount of water
 
+import time
+
+
+print "Starting Countdown"
+
+
+
+# Countdown
+#   Input:      n   Number of seconds
+def countdown(n):
+    while n > 0:
+        print (n)
+        led_shine(n)
+        n -= 1
+        time.sleep(1)
+    print "countdown done"
+    
+# Light up LED
+#   Input:      x   LED x
+#   Output:     LED lights up
+def led_shine(x):
+    print "LED " + str(x) + " lights up!"
+
+countdown(5)
